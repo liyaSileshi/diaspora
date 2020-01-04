88dd1e0a760533fe57c54b36b8cfc8b503dea2cc
liyaSileshi <liyasileshi@gmail.com>
2020-01-03 18:16:08 -0600

    trouble with 3rd party app

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
-    # 'location_field.apps.DefaultConfig',
+    'location_field.apps.DefaultConfig',
 ]
 
 MIDDLEWARE = [
--- events/models.py
+++ events/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-# from location_field.models.plain import PlainLocationField
+from location_field.models.plain import PlainLocationField
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
     date_time = models.DateTimeField('event date time')
-    city = models.CharField(max_length=255)
+    # city = models.CharField(max_length=255)
     # location = PlainLocationField(based_fields=['city'], zoom=7)
