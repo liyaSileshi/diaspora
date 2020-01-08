b78373267bdc5ebc14bee88cce0f4943b0de7017
liyaSileshi <liyasileshi@gmail.com>
2020-01-07 23:21:50 -0800

    home page app

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
     'events',
     'location_field.apps.DefaultConfig',
     'accounts',
--- diaspora/urls.py
+++ diaspora/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')), 
     path('events/', include('events.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
