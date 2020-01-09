8117df989193b4883724535aee559573dfd5aefe
liyaSileshi <liyasileshi@gmail.com>
2020-01-09 15:48:28 -0800

    adding styles

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home/index.html')
 
 def quiz(request):
     return render(request, 'home/quiz_game.html')
